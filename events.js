b804914d9791bcf8bcdecc51e17f29609c630672
Sterling Houghton <sterlinghoughton@gmail.com>
2017-03-22 22:58:10 -0700

    create events.js, responds to slack with challenge for events

---
+++ events.js
@@ -0,0 +1,21 @@
+
+const getSlackEvent = event => ({ slack: JSON.parse(event.body) });
+
+const respond = callback => (event) => {
+  const response = { statusCode: 200 };
+  if (event.slack.type === 'url_verification') {
+    response.body = event.slack.challenge;
+  }
+  callback(null, response);
+  return event;
+};
+
+module.exports.handler = (event, context, callback) => 
+  Promise.resolve(event) // Start promise chain
+    .then(getSlackEvent) // Get Slack event payload
+    .then(respond(callback)) // Respond OK to Slack
+    // .then(verifyToken) // Verify the token
+    // .then(getTeam) // Get team data from DDB
+    // .then(checkForMention) // Check message contains a mention of our bot
+    // .then(invokeAction) // Invoke action function
+    .catch(callback);
