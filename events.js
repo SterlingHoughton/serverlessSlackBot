d378c443fe4ba68a75ab67f9fe11ce321213aa8e
Sterling Houghton <sterlinghoughton@gmail.com>
2017-03-22 23:04:14 -0700

    verify token with slack, add verification token to yml file

--- events.js
+++ events.js
@@ -10,11 +10,18 @@ const respond = callback => (event) => {
   return event;
 };
 
+const verifyToken = (event) => {
+  if (event.slack.token !== process.env.VERIFICATION_TOKEN) {
+    throw new Error('InvalidToken');
+  }
+  return event;
+};
+
 module.exports.handler = (event, context, callback) => 
   Promise.resolve(event) // Start promise chain
     .then(getSlackEvent) // Get Slack event payload
     .then(respond(callback)) // Respond OK to Slack
-    // .then(verifyToken) // Verify the token
+    .then(verifyToken) // Verify the token
     // .then(getTeam) // Get team data from DDB
     // .then(checkForMention) // Check message contains a mention of our bot
     // .then(invokeAction) // Invoke action function
