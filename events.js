0291651081a91ab51c6ffd204f0b0f95ad05011c
Sterling Houghton <sterlinghoughton@gmail.com>
2017-03-23 20:54:03 -0700

    create getTeam, checkForMention, and invokeAction functions in events.js. Uncomment calls in promise chain.

--- events.js
+++ events.js
@@ -1,4 +1,7 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient(); 
 
+const lambda = new AWS.Lambda();
 const getSlackEvent = event => ({ slack: JSON.parse(event.body) });
 
 const respond = callback => (event) => {
@@ -17,12 +20,47 @@ const verifyToken = (event) => {
   return event;
 };
 
+const getTeam = (event) => {
+  const params = {
+    TableName: process.env.TEAMS_TABLE,
+    Key: {
+      team_id: event.slack.team_id,
+    },
+  };
+  console.log('dynamodb.get', params);
+  return dynamodb.get(params).promise()
+    .then(data => Object.assign(event, { team: data.Item }));
+};
+
+const checkForMention = (event) => {
+  const message = event.slack.event.text;
+  const botUserId = event.team.bot.bot_user_id;
+  const botUserIsMentioned = new RegExp(`^<@${botUserId}>.*$`);
+  if (botUserIsMentioned.test(message)) {
+    console.log(`Bot ${botUserId} is mentioned in "${message}"`);
+    return event;
+  }
+};
+
+//Invoke action endpoint, if valid request.
+const actionFunctionName = `${process.env.NAMESPACE}-actions`;
+const invokeAction = (event) => {
+  if (!event) return null;
+  console.log(`Invoking ${actionFunctionName} with event`, event);
+  return lambda.invoke({
+    FunctionName: actionFunctionName,
+    InvocationType: 'Event',
+    LogType: 'None',
+    Payload: JSON.stringify(event),
+  }).promise();
+};
+
 module.exports.handler = (event, context, callback) => 
   Promise.resolve(event) // Start promise chain
     .then(getSlackEvent) // Get Slack event payload
     .then(respond(callback)) // Respond OK to Slack
     .then(verifyToken) // Verify the token
-    // .then(getTeam) // Get team data from DDB
-    // .then(checkForMention) // Check message contains a mention of our bot
-    // .then(invokeAction) // Invoke action function
+    .then(getTeam) // Get team data from DDB
+    .then(checkForMention) // Check message contains a mention of our bot
+    .then(invokeAction) // Invoke action function
     .catch(callback);
